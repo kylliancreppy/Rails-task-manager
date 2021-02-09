4a99c3be92b7d85f11f68a670c83368347e02174
Kyllian Creppy <kyllian.creppy@outlook.fr>
2021-02-09 11:54:27 +0100

    Add details and completed to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_09_102617) do
+ActiveRecord::Schema.define(version: 2021_02_09_102947) do
 
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+    t.text "details"
+    t.boolean "completed", default: false
   end
 
 end
